804582f2fd80fdd5b70823869d723959aea413a0
Addison Dalton <zarch14@gmail.com>
2019-03-15 14:37:45 -0400

    practice with event handling

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer';
+        const options = ['Thing one', 'Thing two', 'Thing three'];
+
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle} />
                 <Action />
-                <Options />
+                <Options options={options} />
                 <AddOption />
             </div>
         );
@@ -15,29 +19,44 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
 }
 
 class Action extends React.Component {
+    handlePick() {
+        console.log('handle pick');
+    }
+
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+
+    handleRemoveAll() {
+        alert('remove all');
+    }
+
     render() {
         return (
             <div>
-                <p>These are your options:</p>
-               <Option />
+                <button onClick={this.handleRemoveAll}>Remove All</button>
+                {
+                    this.props.options.map(option => <Option key={option} optionText={option}/>)
+                }
             </div>
         );
     }
@@ -46,16 +65,32 @@ class Options extends React.Component {
 class Option extends React.Component {
     render() {
         return (
-            <p>This is an option</p>
+            <p>{this.props.optionText}</p>
         );
     }
 }
 
 class AddOption extends React.Component {
+
+
+
+    handleAddOption(event) {
+        event.preventDefault();
+
+        const option = event.target.elements.option.value;
+
+        if (option) {
+            alert(option);
+        }
+    }
+
     render() {
         return (
             <div>
-                <p>Add option</p>
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option" />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
